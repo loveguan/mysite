7d6d957c2eb6cb71adae9a13ad15bea2253ff14c
joj <zhouguanjie@qq.com>
2017-08-22 22:34:01 +0800

    格式化输出，判断输入是否为整数的形式

--- ceshi/week2/day01/format.py
+++ ceshi/week2/day01/format.py
@@ -26,13 +26,13 @@ job = input("job:")
 salary = input("salary:")
 
 # 法一，判断输入的是否像整数
-'''
+
 if salary.isdigit():
     salary=int(salary)
 else:
     print("you must input digest")
     exit("wrong,digest")
-'''
+
 #  法二 或者在这里可以适用%d来获取数字，如果传入的字符串不为数字则报错
 
 msg = '''
