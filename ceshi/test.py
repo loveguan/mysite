c3e7747460a21b3d54bb814b7463cd082ca2dd80
joj <zhouguanjie@qq.com>
2017-08-22 21:56:26 +0800

    九九

---
+++ ceshi/test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+'''
+
+@author: JOJ
+
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+
+@contact: zhouguanjie@qq.com
+
+@software: JOJ
+
+@file: test.py
+
+@time: 2017/8/20 18:59
+
+@desc:
+
+'''
+
+
+
+"""
+death_age = 80
+age = input('your age:')
+print('you can live for ',death_age-int(age),'years')
+print('you can live for '+ str(death_age-int(age))+' years')
+"""
+
+#
+# age_of_princal = 56
+#
+# gusess_age=int(input(">>:"))
+# if gusess_age == age_of_princal:
+#     print('ok!')
+# else:
+#     print('wrongs')
+
+print(1,end=' ')
+print(2)
+
+
